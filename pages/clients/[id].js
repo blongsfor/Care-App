b77a0b5a4bfc31f1f86262f3a0988a020b4ff24f
Björn <bjoern.n@mailbox.org>
2024-06-06 22:22:16 +0200

    change folder structure

--- pages/clients/[id].js
+++ pages/clients/[id].js
@@ -1,5 +1,5 @@
-import dbConnect from "../../../lib/dbConnect";
-import Client from "../../../lib/models/client";
+import dbConnect from "../../lib/dbConnect";
+import Client from "../../lib/models/client";
 
 export default async function handler(request, response) {
   await dbConnect();
