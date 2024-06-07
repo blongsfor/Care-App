8df84e111abe28da4da1eba307c9129c1eb95aab
Björn <bjoern.n@mailbox.org>
2024-06-07 16:25:57 +0200

    pictures working and loading

--- src/components/ClientList.jsx
+++ src/components/ClientList.jsx
@@ -1,10 +1,11 @@
-// components/ClientList.jsx
+import Image from "next/image";
 
 export default function ClientList({ clients }) {
   return (
     <ul>
       {clients.map((client) => (
         <li key={client._id}>
+          <Image src={client.picture} width={200} height={200} />
           <h2>
             {client.firstName} {client.lastName}
           </h2>
