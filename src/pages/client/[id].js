48f7add2dc6874a3abd8e291d0abc167e0cd33e9
Björn <bjoern.n@mailbox.org>
2024-06-09 21:44:29 +0200

    link to detailspage working (without pictures)

---
+++ src/pages/client/[id].js
@@ -0,0 +1,17 @@
+// pages/clients/[id].js
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import Client from "../../components/Client"; // Adjust path if necessary
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function ClientDetails() {
+  const router = useRouter();
+  const { id } = router.query;
+  const { data, error } = useSWR(id ? `/api/clients/${id}` : null, fetcher);
+
+  if (error) return <div>Failed to load client</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return <Client {...data} />;
+}
