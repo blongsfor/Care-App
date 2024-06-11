77778ca3b5ae1593b280b411c8a82fa66c21d855
Björn <bjoern.n@mailbox.org>
2024-06-11 11:10:18 +0200

    add button to entries page

--- src/pages/entries.js
+++ src/pages/entries.js
@@ -3,6 +3,7 @@ import useSWR from "swr";
 import EntryList from "../components/EntryList";
 import BackButton from "@/components/BackButton";
 import LogoutButton from "@/components/LogoutButton";
+import DocumentButton from "@/components/DocumentButton";
 
 export default function Entries() {
   const { data, error } = useSWR("/api/entries");
@@ -14,6 +15,7 @@ export default function Entries() {
     <>
       <LogoutButton />
       <BackButton />
+      <DocumentButton />
       <div>
         <h2>Entries</h2>
         <EntryList entries={data} />
